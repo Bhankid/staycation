79131976c5a80652d9335055837e15c54a5c6c56
Bhankid <alfredfianyo50@gmail.com>
2025-01-25 04:49:28 +0000

    update 101

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,9 @@
+"use client";
+import { useState } from "react";
+
 function Header() {
+  const [activeTab, setActiveTab] = useState("Home");
+
   return (
     <div className="container mx-auto p-4">
       <div className="flex justify-between items-center p-4 border-b">
@@ -7,16 +12,40 @@ function Header() {
           <span className="text-gray-900">cation.</span>
         </div>
         <nav className="space-x-4">
-          <a href="#" className="text-blue-600">
+          <a
+            href="#"
+            className={`text-blue-600 hover:text-blue-700 transition duration-300 cursor-pointer ${
+              activeTab === "Home" ? "font-bold" : "font-normal"
+            }`}
+            onClick={() => setActiveTab("Home")}
+          >
             Home
           </a>
-          <a href="#" className="text-gray-900">
+          <a
+            href="#"
+            className={`text-gray-900 hover:text-blue-600 transition duration-300 cursor-pointer ${
+              activeTab === "Browse by" ? "font-bold" : "font-normal"
+            }`}
+            onClick={() => setActiveTab("Browse by")}
+          >
             Browse by
           </a>
-          <a href="#" className="text-gray-900">
+          <a
+            href="#"
+            className={`text-gray-900 hover:text-blue-600 transition duration-300 cursor-pointer ${
+              activeTab === "Stories" ? "font-bold" : "font-normal"
+            }`}
+            onClick={() => setActiveTab("Stories")}
+          >
             Stories
           </a>
-          <a href="#" className="text-gray-900">
+          <a
+            href="#"
+            className={`text-gray-900 hover:text-blue-600 transition duration-300 cursor-pointer ${
+              activeTab === "Agents" ? "font-bold" : "font-normal"
+            }`}
+            onClick={() => setActiveTab("Agents")}
+          >
             Agents
           </a>
         </nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Family from "./components/Family";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div className="mt-24">
+      <div className="mt-24 pt-20">
         <Hero />
       </div>
       <div className="mt-24 pt-20">
